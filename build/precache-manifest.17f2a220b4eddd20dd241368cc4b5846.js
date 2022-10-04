5ffa6ae2af98a49114f2cc5db93ef9046596221d
Micheal A <aad3rinto@gmail.com>
2022-10-04 17:38:28 +0000

    add yml file

---
+++ build/precache-manifest.17f2a220b4eddd20dd241368cc4b5846.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4b9d4cc67c994313cf476671c27785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1abbf52ea3c6c182b426",
+    "url": "/static/js/2.8116e639.chunk.js"
+  },
+  {
+    "revision": "a841eaea23006d9a179d",
+    "url": "/static/js/main.c963150e.chunk.js"
+  },
+  {
+    "revision": "7ebe2cd53892b00c3fe6",
+    "url": "/static/js/runtime-main.9203f476.js"
+  }
+]);
